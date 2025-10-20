b216c09ed7fa90877560c3bf1eff83e8755f38b2
Jyoti Gautam <jyotigautam@Jyotis-MacBook-Pro.local>
2025-10-20 23:22:34 +0530

    Add pgvector types to production config

--- lib/financial_advisor_ai/repo.ex
+++ lib/financial_advisor_ai/repo.ex
@@ -2,10 +2,4 @@ defmodule FinancialAdvisorAi.Repo do
   use Ecto.Repo,
     otp_app: :financial_advisor_ai,
     adapter: Ecto.Adapters.Postgres
-
-  @impl true
-  def init(_type, config) do
-    # Configure pgvector types
-    {:ok, Keyword.put(config, :types, Pgvector.Ecto.Types)}
-  end
 end
